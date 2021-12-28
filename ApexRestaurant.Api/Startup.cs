8981827dc31ef13b9f95b8397c807691a1b70488
Ayush <ayushkc9@gmail.com>
2021-12-28 07:44:55 +0545

    error-encountered

--- ApexRestaurant.Api/Startup.cs
+++ ApexRestaurant.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApexRestaurant.Repository;
+using ApexRestaurant.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,11 @@ namespace ApexRestaurant.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            RepositoryModule.Register(services,
+                Configuration.GetConnectionString("DefaultConnection"),
+                GetType().Assembly.FullName);
+            ServicesModule.Register(services);
             services.AddControllers();
         }
 
