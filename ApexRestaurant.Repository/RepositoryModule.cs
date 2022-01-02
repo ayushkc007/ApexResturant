bc284a6115d7b28448e7492922bd5bc90086e585
Ayush <ayushkc9@gmail.com>
2022-01-02 07:17:47 +0545

    added_dependencies[SQLserver-left]

--- ApexRestaurant.Repository/RepositoryModule.cs
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -1,7 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ApexRestaurant.Repository.RCustomer;
-
+using ApexRestaurant.Repository.RMeal;
+using ApexRestaurant.Repository.RMealDish;
+using ApexRestaurant.Repository.RMenuItem;
+using ApexRestaurant.Repository.RStaffRole;
+using ApexRestaurant.Repository.RStaff;
+using ApexRestaurant.Repository.RMenu;
 
 namespace ApexRestaurant.Repository
 {
@@ -12,6 +17,12 @@ namespace ApexRestaurant.Repository
         {
             services.AddDbContext<RestaurantContext>(options => options.UseMySQL(connection, builder => builder.MigrationsAssembly(migrationsAssembly)));
             services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<IMealRepository, MealRepository>();
+            services.AddTransient<IMealDishRepository, MealDishRepository>();
+            services.AddTransient<IMenuItemRepository, MenuItemRepository>();
+            services.AddTransient<IMenuRepository, MenuRepository>();
+            services.AddTransient<IStaffRepository, StaffRepository>();
+            services.AddTransient<IStaffRoleRepository, StaffRoleRepository>();
         }
     }
 }
